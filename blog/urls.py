d4c359d95620643bef6e22c88104516d5ecccb51
cchrispivy <cchrisleepyles@ivytech.edu>
2022-07-15 09:59:58 -0400

    Add user profile and picture functionality

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
@@ -13,4 +12,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('app.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
