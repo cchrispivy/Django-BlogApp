0ed00525a83cb6b891a6b8c0781de693930d0d8e
cchrispivy <cchrisleepyles@ivytech.edu>
2022-07-19 09:23:58 -0400

    Replace path with urls parameter

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from input_mask.widgets import InputMask
 
+
+
+class SSNInput(InputMask):
+    mask = {'mask': '999-99-9999'}
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    ssn = SSNInput()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'ssn', 'street', 'city', 'state', 'zip_code', 'county', 'birth_date', 'phone_number', 'password1', 'password2']
 
