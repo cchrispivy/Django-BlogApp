67688d71321d9a4b6cc944befc9ac71c0e146c6d
cchrispivy <cchrisleepyles@ivytech.edu>
2022-07-19 15:23:09 -0400

    Update requirements.txt

--- blog/settings/common.py
+++ blog/settings/common.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'input_mask'
 ]
 
 MIDDLEWARE = [
--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from input_mask.widgets import InputMask
 
 class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
