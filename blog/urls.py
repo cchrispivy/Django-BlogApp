d08132f8d355167674cb2f21a9f82502d9c37d35
cchrispivy <cchrisleepyles@ivytech.edu>
2022-07-19 10:00:34 -0400

    Undo url/path config changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.conf.urls import url
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('register/', user_views.register, name='register'),
-    url('profile/', user_views.profile, name='profile'),
-    url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    url('', include('app.urls')),
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', include('app.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/forms.py
+++ users/forms.py
@@ -9,10 +9,11 @@ class SSNInput(InputMask):
     mask = {'mask': '999-99-9999'}
 
 class UserRegisterForm(UserCreationForm):
+    pk = forms.IntegerField()
     email = forms.EmailField()
-    ssn = SSNInput()
+    #ssn = SSNInput()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'ssn', 'street', 'city', 'state', 'zip_code', 'county', 'birth_date', 'phone_number', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
