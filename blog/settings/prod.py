de04376c3e9cd48519b554d0a01fd3a8362106dd
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-01 11:28:28 -0400

    Update migrations and prod.py

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -3,7 +3,7 @@ from .common import *
 import dj_database_url
 
 
-DATABASES = {'default': dj_database_url.config(default='postgres://user:pass@localhost/dbname')}
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:postgres@localhost/blogapp_db')}
 
 ALLOWED_HOSTS = ['blogapp-ivy.herokuapp.com']
 
