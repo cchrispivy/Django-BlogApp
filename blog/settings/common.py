0ca46c5c1a92e5ffc76e248b9d35402807fd8427
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-01 08:36:18 -0400

    configure STATIC_ROOT

--- blog/settings/common.py
+++ blog/settings/common.py
@@ -111,6 +111,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
         os.path.join(BASE_DIR, 'static'),        
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR_2, 'media')
 MEDIA_URL = '/media/'
