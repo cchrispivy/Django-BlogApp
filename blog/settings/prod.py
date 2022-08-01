1bdbc6de7c0f283222fcbec9f816af5add12038c
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-01 10:34:36 -0400

    update settings configurations

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -1,6 +1,6 @@
 # TEMP SETTINGS FOR TROUBLESHOOTING #
 from .common import *
 
-ALLOWED_HOSTS = ['blogapp-ivy.herokuapp.com', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['blogapp-ivy.herokuapp.com']
 
 DEBUG = True
