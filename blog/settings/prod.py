8f556b990d90eca59096f1471b3d499028de6b61
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-01 11:10:32 -0400

    update settings configurations

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -4,3 +4,7 @@ from .common import *
 ALLOWED_HOSTS = ['blogapp-ivy.herokuapp.com']
 
 DEBUG = True
+
+
+
+DATABASES = {'default': dj_database_url.config(default='postgres://blogapp_admin:test123@localhost/blogapp_db')}
