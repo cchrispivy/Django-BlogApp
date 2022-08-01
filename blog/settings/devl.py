927bc8c867dec737a233dc848a1d7d8be3360075
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-01 10:07:24 -0400

    update settings configurations

--- blog/settings/devl.py
+++ blog/settings/devl.py
@@ -1,3 +1,19 @@
 """Development settings and globals."""
 from blog.settings.common import *
 
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogapp',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
