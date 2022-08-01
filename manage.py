049fd3ed155dc49a8151d7d1975a95a1e053c8a9
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-01 09:04:14 -0400

    update project to prod settings

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import dotenv
 
 if __name__ == '__main__':
     dotenv.read_dotenv('app.env')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.common')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.prod')
 
     from django.core.management import execute_from_command_line
 
