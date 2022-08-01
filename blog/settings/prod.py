d17382aaa6c82ed565ae42bc1735a9ea49d5bfe9
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-01 10:12:50 -0400

    update prod.py

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -5,15 +5,4 @@ ALLOWED_HOSTS = ['blogapp-ivy.herokuapp.com']
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogapp',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
 
