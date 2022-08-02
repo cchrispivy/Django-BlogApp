f76eced13c4626eeb52ff5f1feab3d228a6012f0
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-02 11:52:42 -0400

    update prod.py

--- blog/settings/common.py
+++ blog/settings/common.py
@@ -96,7 +96,7 @@ STATICFILES_DIRS=(
         os.path.join(BASE_DIR, 'static'),        
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR_2, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
