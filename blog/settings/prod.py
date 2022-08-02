8574fd0daab9c81ee0eb7efbef376815b4db44d7
cchrispivy <cchrisleepyles@ivytech.edu>
2022-08-02 11:31:34 -0400

    update prod.py

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -3,10 +3,13 @@ from .common import *
 import dj_database_url
 
 
-DATABASES = {'default': dj_database_url.config(default='postgres://postgres:postgres@localhost/blogapp_db')}
+#DATABASES = {'default': dj_database_url.config(default='postgres://postgres:postgres@localhost/blogapp_db')}
 
 ALLOWED_HOSTS = ['blogapp-ivy.herokuapp.com']
 
 DEBUG = True
 
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 
